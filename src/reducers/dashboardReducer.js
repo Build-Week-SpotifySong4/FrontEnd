95b268118c94c4afefec5117595b8431b237b617
Seth Cox <Sethcox16@yahoo.com>
2020-03-02 12:02:39 -0500

    Setup file structure with some functions

---
+++ src/reducers/dashboardReducer.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    username: '',
+    password: ''
+}
+
+export const dashboardReducer = ( state = initialState, action ) => {
+    switch(action.type) {
+        default: 
+            state;
+    }
+}
